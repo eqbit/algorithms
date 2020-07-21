53223c19dfc8b45d2d8323a0559d127575773db5
eqbit <eqbits@gmail.com>
2020-07-21 10:42:47 +0500

    heapsort

---
+++ sort/heap/index.js
@@ -0,0 +1,50 @@
+const inputArray = [3, 7, 1, 8, 4, 6, 5, 2, 0, 9, 10, 123, -2, 233, 2, 233, 21, 7, -45, 56, 2, 5, 8, 1];
+
+class Heap {
+  constructor(inputArray) {
+    this.inputArray = inputArray;
+  }
+  
+  swap(index1, index2) {
+    const temp = inputArray[index1];
+    this.inputArray[index1] = this.inputArray[index2];
+    this.inputArray[index2] = temp;
+  }
+  
+  heapify(rootIndex, length = this.inputArray.length, array = this.inputArray) {
+    const leftIndex = rootIndex * 2 + 1;
+    const rightIndex = rootIndex * 2 + 2;
+    let largestIndex = rootIndex;
+  
+    if (leftIndex < length && array[leftIndex] > array[largestIndex]) {
+      largestIndex = leftIndex;
+    }
+  
+    if (rightIndex < length && array[rightIndex] > array[largestIndex]) {
+      largestIndex = rightIndex;
+    }
+  
+    if (rootIndex !== largestIndex) {
+      this.swap(rootIndex, largestIndex);
+      this.heapify(largestIndex, length, array);
+    }
+  }
+  
+  sort() {
+    for (let i = Math.floor(this.inputArray.length / 2) - 1; i >= 0; i--) {
+      this.heapify(i);
+    }
+  
+    console.log(this.inputArray);
+    
+    for (let i = this.inputArray.length - 1; i > 0; i--) {
+      this.swap(i, 0);
+      this.heapify(0, i);
+    }
+  
+    console.log(this.inputArray);
+  }
+}
+
+const heap = new Heap(inputArray);
+heap.sort();
